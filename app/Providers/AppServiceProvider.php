b09f7e26f4463b29b3aa2b7e03898cc1bef6ef5d
issqTK <issq.tk.91@gmail.com>
2023-08-25 10:58:48 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,9 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-       /*  if(config('app.env') === 'production') {
+        if(config('app.env') === 'production') {
             \URL::forceScheme('https');
-        } */
+        }
     }
 
     /**
