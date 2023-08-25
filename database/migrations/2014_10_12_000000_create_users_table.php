4f11eb749df55c66910f225fe7d81a2deb720686
issqTK <issq.tk.91@gmail.com>
2023-08-25 10:29:01 +0100

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             DB::statement('SET SESSION sql_require_primary_key=0');
-            $table->id()->primary();
+            $table->id();
             $table->boolean('type')->default(false);
             $table->string('name');
             $table->string('email')->unique();
