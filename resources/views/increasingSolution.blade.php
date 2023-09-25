b942f02fc2129ee872577d6d6933650a747226e0
issqTK <issq.tk.91@gmail.com>
2023-09-25 08:32:13 +0100

    Update increasingSolution.blade.php

--- resources/views/increasingSolution.blade.php
+++ resources/views/increasingSolution.blade.php
@@ -5,15 +5,11 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <title>Augmentation de votre facture d'électricité de 10% - Voici une solution intéressante !</title>
         <style>
-            *{ box-sizing:border-box; margin:0;padding:0 }
+            *{ box-sizing:border-box; margin:0; padding:0 }
 
             body { margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #535353;color: #000000; font-family: Arial}
             
             #primary-table { width:100%; max-width:600px; background-color:#e8eae9; margin:0 auto }
-            
-            #footer { width:100%; max-width:600px; background-color:#1e1e1e; color:white; margin:0 auto; padding:35px}
-            
-            #footer td { border-collapse: collapse !important;vertical-align: middle; padding:10px 0 }
 
             #primary-table > tbody > tr { vertical-align: top;  }
 
@@ -21,20 +17,25 @@
 
             h1 { background-color: #11b793; padding:25px 15px; text-align:center; font-size:1.3em; color:white;}
 
-            #content {}
+            /*  */
 
-            #separate{ display:block; background-color:#1e1e1e; height: 15px; border-radius:5px}
+            #separate{ display:block; background-color:#1e1e1e; height: 15px; border-radius:5px }
 
-            #content > div { background:white; width:96%; margin:auto; padding:25px;}
+            #content > div { background:white; width:96%; margin:auto; padding:25px; }
 
-            #content > div > p { text-align: justify; letter-spacing: .2px; padding: 8px; line-height: 30px; font-size: 1em; }
-            #content > div > p span { display:block ;margin-bottom:20px}
+            #content > div > p { word-break:keep-all; letter-spacing: .2px; line-height: 28px; font-size: 1em; margin-bottom:8px}
 
             #content > div > p a { color:#0284c7; text-decoration:none; }
 
             #visit{ display:table; margin: 25px auto ;padding:15px 25px; color:white; background-color:#0284c7; text-decoration:none; text-transform:uppercase; font-size:.9em }
 
-            .img-socieux{ margin-right:8px; }
+            /*  */
+             
+            #footer { width:100%; max-width:600px; background-color:#1e1e1e; color:white; margin:0 auto; padding:35px}
+            
+            #footer td { border-collapse: collapse !important;vertical-align: middle; padding:10px 0 }
+
+            .img-socieux{ margin:8px 8px 0 0; }
             
             .img-socieux img{ width:30px }
             
@@ -57,29 +58,38 @@
                             <td><h1>NOUS SOMMES À VOS CÔTÉS</h1></td>
                         </tr>
                         <tr>
-                            <td><img src="https://lenergia.fr/images/logo.png" width="100%"></td>
+                            <td><img src="https://lenergia.fr/images/logo.png" width="100%" draggable="false"></td>
                         </tr>
                         <tr>
                             <td id="content">
                                 <span id="separate"></span>
 
                                 <div>
-                                    <p>
-                                        <span><strong>Bonjour cher(e) {{$name}}</strong></span>
-                                        <span>J'espère que vous allez bien et que votre rentrée se passe agréablement !</span>
-                                        <span>Nous nous permettons de vous écrire car nous avons récemment remarqué que votre facture d'électricité a connu une hausse (<em>augmentation de 10%</em> du tarif d'électricité depuis le 2 août 2023). </span>
-                                        <span>Nous comprenons que cela peut être stressant de voir les coûts augmenter, et nous voulions vous informer <strong>d'une solution</strong> qui pourrait vous être bénéfique !</span>
-                                        <span>Alors, Avez-vous déjà pensé à <strong>l'énergie solaire</strong> pour alimenter votre maison ?</span>
-                                        <span>L'énergie solaire est une option écologique et durable qui peut vous permettre de réduire votre dépendance aux tarifs électriques qui n'arrête pas d'augmenter.</span>
-                                        <span>Optez pour <strong>une solution de panneaux solaires</strong> pourrait vous aider à produire votre propre énergie, ce qui vous offrira une importante indépendance énergétique et pourrait même vous donner le privilège de vendre le surplus d'électricité.</span>
-                                        <span>Bien sûr, je comprends que c'est une décision importante, et je ne souhaite pas vous pousser à quoi que ce soit ;</span> 
-                                        <span>Je voulais simplement partager cette idée avec vous, car cela pourrait être une opportunité intéressante pour réduire vos coûts d'électricité.</span>
-                                        <span>Si vous souhaitez en savoir plus sur <strong>l'énergie solaire</strong> ou si vous avez des questions à ce sujet, n'hésitez pas à nous contacter. </span>
-                                        <span>Je serais ravi de vous fournir plus d'informations ou de vous mettre en relation avec un expert en la matière, </span>
-                                        <span>Ou bien, <a href="https://lenergia.fr/" ><strong>VISITEZ NOTRE SITE</strong></a> pour plus de renseignement.</span>
-                                    </p>
-
-
+                                    
+                                    <p><strong>Bonjour cher(e) {{$name}}</strong></p>
+                                    
+                                    <p>J'espère que vous allez bien et que votre rentrée se passe agréablement !</p>
+                                    
+                                    <p>Nous nous permettons de vous écrire car nous avons récemment remarqué que votre facture d'électricité a connu une hausse (<em>augmentation de 10%</em> du tarif d'électricité depuis le 2 août 2023). </p>
+                                    
+                                    <p>Nous comprenons que cela peut être stressant de voir les coûts augmenter, et nous voulions vous informer <strong>d'une solution</strong> qui pourrait vous être bénéfique !</p>
+                                    
+                                    <p>Alors, Avez-vous déjà pensé à <strong>l'énergie solaire</strong> pour alimenter votre maison ?</p>
+                                    
+                                    <p>L'énergie solaire est une option écologique et durable qui peut vous permettre de réduire votre dépendance aux tarifs électriques qui n'arrête pas d'augmenter.</p>
+                                    
+                                    <p>Optez pour <strong>une solution de panneaux solaires</strong> pourrait vous aider à produire votre propre énergie, ce qui vous offrira une importante indépendance énergétique et pourrait même vous donner le privilège de vendre le surplus d'électricité.</p>
+                                    
+                                    <p>Bien sûr, je comprends que c'est une décision importante, et je ne souhaite pas vous pousser à quoi que ce soit ;</p>
+                                    
+                                    <p>Je voulais simplement partager cette idée avec vous, car cela pourrait être une opportunité intéressante pour réduire vos coûts d'électricité.</p>
+                                    
+                                    <p>Si vous souhaitez en savoir plus sur <strong>l'énergie solaire</strong> ou si vous avez des questions à ce sujet, n'hésitez pas à nous contacter. </p>
+                                    
+                                    <p>Je serais ravi de vous fournir plus d'informations ou de vous mettre en relation avec un expert en la matière, </p>
+                                    
+                                    <p>Ou bien, <a href="https://lenergia.fr/" ><strong>VISITEZ NOTRE SITE</strong></a> pour plus de renseignement.</p>
+                                    
                                     <a href="https://lenergia.fr/" id="visit"><strong>Rendez nous visite</strong></a>
                                 </div>
                             </td>
