6d4dfa6fe7454f2097047ed4cc7dfb5984572eae
issqTK <issq.tk.91@gmail.com>
2024-01-05 14:28:12 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,11 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('down', function() {
+    \Artisan::call('down');
+    echo 'DONE';
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     
